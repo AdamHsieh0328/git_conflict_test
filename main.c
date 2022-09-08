d51a6659c507dac23b8ad43298516c71ddef4036
Adam CJ Hsieh/WHQ/Wistron <Adam_CJ_Hsieh@wistron.com>
2022-09-08 11:09:10 +0800

    Add line in CB

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main(){
 	printf("this is main.c in computer A");
 	printf("hi i modify the main.c ~~ in computer A");
 	printf("Hello world in computer B");
+	printf("Add line in computer B");
 	return 0;
 }
